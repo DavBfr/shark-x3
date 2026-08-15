8b8d0742e81747bd60274c0697074cd64da5682a
David PHAM-VAN <dev.nfet.net@gmail.com>
2026-08-15 09:51:13 -0300

    fix: update project configuration and references to "Shark X3 Config"

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"shark_x3", origin, size)) {
+  if (!window.Create(L"Shark X3 Config", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
